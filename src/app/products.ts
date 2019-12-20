32cf5c4c8e25e8e10926450e70e17a309e4189b9
GoBigRed1994 <artie3.meyers@gmail.com>
2019-12-20 09:52:30 -0600

    3 files modified, 3 added

--- src/app/products.ts
+++ src/app/products.ts
@@ -6,12 +6,12 @@ export const products = [
   },
   {
     name: 'Phone Mini',
-    price: 699,
+    price: 6699,
     description: 'A great phone with one of the best cameras'
   },
   {
     name: 'Phone Standard',
-    price: 299,
+    price: 2939,
     description: 'You dont want this one'
   }
 ];
